ff096d976ffbd572f9aa169cc73b58b682976bdc
Sippora04 <sippora.toppo95@gmail.com>
2020-12-28 02:17:19 +0530

    Refactored: change on code

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -9,6 +9,7 @@ import lombok.Data;
 
 public @Data class EmployeePayrollDTO {
 
+	// @NotEmpty(message="Employee name cannot be null");
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$)?$", message = "Employee name is Invalid")
 	public String name;
 	@Min(value = 500, message = "Min wage should be more than 500")
