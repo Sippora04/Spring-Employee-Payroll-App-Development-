ee3625119bb774e3e7f3d862d51c849ea3fd59f8
Sippora04 <sippora.toppo95@gmail.com>
2020-12-28 00:02:27 +0530

    Added: (3.2) using Lombok Library for Logging

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmpPayrollApplication.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class EmpPayrollApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(EmpPayrollApplication.class, args);
+		
+		log.info("Employee Payroll Application Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+	}
+
+}
